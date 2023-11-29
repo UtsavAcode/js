66bae8118123eed13ccd0a4bbbe29bf7d4bc3df8
utsav <utsavadhikari452@gmail.com>
2023-11-29 18:06:42 +0545

    add to list

--- script/events.js
+++ script/events.js
@@ -1,19 +1,59 @@
-// this is the input operation on the form that i created 
+// the firs thing to do is to list out all the ui that will take part in the event 
 
-let val = document.querySelector('form');
-
-// val.addEventListener('submit', sub);
-
-// function sub(submit){
-//     let text = document.getElementById('entry').value;
-//     let items = [text];
-
-//     items.forEach(function(item,index){
-//         let show  = document.querySelector('tr');
-//         show.innerHTML+=`<td class="collection-item d-flex justify-content-between align-items-center">${item}<i class="bi bi-x-lg"></i></td>`
-                        
-//     });
+const form = document.querySelector('#task-form');
+const input = document.querySelector('#entry');
+const taskList = document.querySelector('.collection');
+const clear = document.querySelector('#clear');
 
 
-//     submit.preventDefault();
-// }
+//loading the eventListeners
+
+loadEventListener();
+
+//creating the events 
+
+function loadEventListener(){
+    form.addEventListener('submit', addTask);
+}
+
+function addTask(e){
+
+    if(input === ''){
+        alert("Add a task");
+    }
+
+    //createt a td element to display the added task 
+
+    const td = document.createElement('td');
+
+    //give the element a className
+    td.className = "collection-item";
+
+    //create a text node and append to td 
+    text_node = document.createTextNode(input.value);
+    td.appendChild(text_node);
+
+    //create a new link element 
+    const link = document.createElement('a');
+
+    //add class to the element 
+    link.className='clear';
+
+    //if you want to add a new html element
+    link.innerHTML = "<i class='clear bi bi-x-lg'></i>";
+
+    // append the created element to td 
+    td.appendChild(link);
+
+    //append td to tr 
+
+    taskList.appendChild(td);
+
+ 
+    //clear the input field 
+
+    input.value = '';
+
+
+    e.preventDefault();
+}
