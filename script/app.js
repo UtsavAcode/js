405f7f6c0b562ccf5ee86d1f7dd02b85781f8446
utsav <utsavadhikari452@gmail.com>
2024-05-28 21:35:48 +0545

    challenges

--- script/app.js
+++ script/app.js
@@ -11,3 +11,20 @@ document.addEventListener("DOMContentLoaded", function () {
     remains.innerHTML = maxLength - currentLength;
   });
 });
+
+function daysToLive(age) {
+  var TimeToLive = 90 - age;
+  var days = TimeToLive * 365;
+  var weeks = TimeToLive * 52;
+  var months = TimeToLive * 12;
+
+  console.log(
+    "You have " + days + " days " + weeks + " weeks and " + months + " to live."
+  );
+}
+
+daysToLive(89);
+
+function milk(money){
+  
+}
