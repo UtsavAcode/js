b4e7a72934056e6bab33f120a0d08dbdb275bac1
utsav <utsavadhikari452@gmail.com>
2024-05-31 21:45:59 +0545

    the leap year issue fixed

--- script/app.js
+++ script/app.js
@@ -64,7 +64,7 @@ console.log("This is a random number " + n);
 //  alert("This is the amount of love you will have: " + love + "%");
 
 function leapYear(year) {
-  if ((year % 4 === 0 && year % 10 !== 0) || year % 400 === 0) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
     console.log("The year " + year + " is a leap year");
   } else {
     console.log("This year is not a leap year.");
@@ -73,4 +73,4 @@ function leapYear(year) {
   return year;
 }
 
-leapYear(2016);
+leapYear(2020);
